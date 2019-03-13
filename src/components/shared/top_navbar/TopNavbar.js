7c8e5edc11ec502703123fc2856968685cd5f5fc
Ayush Shekhar <ayush221b@gmail.com>
2019-03-13 20:25:05 +0530

    Created Navbar

--- src/components/shared/top_navbar/TopNavbar.js
+++ src/components/shared/top_navbar/TopNavbar.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+// Import component stylesheet
+import './TopNavbar.css';
 
 export default class TopNavbar extends Component {
     render() {
         return (
-        <div>
-            Hello from Navbar!
+        <div className='navbar'>
+            <ul>
+                <li>
+                    <Link 
+                        className = 'link' 
+                        style={{ textDecoration: 'none' }}
+                        to='/'
+                    >
+                    Phonebook Directory
+                    </Link>
+                </li>
+            </ul>
         </div>
         )
     }
