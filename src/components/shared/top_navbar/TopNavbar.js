5b44d3c683687d78fe9f919042629d144f4a391d
Ayush Shekhar <ayush221b@gmail.com>
2019-03-13 20:30:04 +0530

    converted topnavbar to functional component

--- src/components/shared/top_navbar/TopNavbar.js
+++ src/components/shared/top_navbar/TopNavbar.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 // Import component stylesheet
 import './TopNavbar.css';
 
-export default class TopNavbar extends Component {
-    render() {
-        return (
-        <div className='navbar'>
-            <ul>
-                <li>
-                    <Link 
-                        className = 'link' 
-                        style={{ textDecoration: 'none' }}
-                        to='/'
-                    >
-                    Phonebook Directory
-                    </Link>
-                </li>
-            </ul>
-        </div>
-        )
-    }
+const TopNavbar = () => {
+
+    return (
+    <div className='navbar'>
+        <ul>
+            <li>
+                <Link 
+                    className = 'link' 
+                    style={{ textDecoration: 'none' }}
+                    to='/'
+                >
+                Phonebook Directory
+                </Link>
+            </li>
+        </ul>
+    </div>
+    );
 }
+
+export default TopNavbar;
