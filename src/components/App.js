02ce9280f77be84ecca96e39c0a78f4edb683eed
Ayush Shekhar <ayush221b@gmail.com>
2019-03-13 19:59:34 +0530

    Basic setup for all components, setup routing, changed meta values in index.html

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
+// Import React essentials
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Import the Components for different routes(pages)
+import Dashboard from './pages/dashboard/Dashboard';
+import Add from './pages/add/Add';
+import TopNavbar from "./shared/top_navbar/TopNavbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello Phonebook!
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <TopNavbar/>
+          <Switch>
+            <Route path='/' exact component={Dashboard} />
+            <Route path='/add' component={Add} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/shared/top_navbar/TopNavbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TopNavbar extends Component {
+    render() {
+        return (
+        <div>
+            Hello from Navbar!
+        </div>
+        )
+    }
+}
