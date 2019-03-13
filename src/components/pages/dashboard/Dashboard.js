8006a399671b609048aed63b6b59c0f02457e3cb
Ayush Shekhar <ayush221b@gmail.com>
2019-03-13 22:25:19 +0530

    Implemented Dashboard page layout

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom'
+
+// Import Component Style
+import './Dashboard.css';
 
 export default class Dashboard extends Component {
+
+    onAddButtonClick = () => {
+        alert('Add button was clicked');
+    }
+
     render() {
+
+        const Button = withRouter(({ history }) => (
+            <button
+                className='add-button'
+                type="button" 
+                onClick={() => { history.push('/add') }}
+            >
+                ADD
+            </button>
+        ));
+
         return (
-        <div>
-            Hello from Dashboard!
+        <div className='dashboard'>
+            <Button/>
+            <div className="container">
+                <div className="name">
+                    NAME
+                </div>
+                <div className="phone">
+                    PHONE
+                </div>
+            </div>
         </div>
-        )
+        );
     }
 }
