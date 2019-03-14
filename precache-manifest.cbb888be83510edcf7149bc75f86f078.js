7f6dd1eefeeb9c2ef99048b834a2ba8fa46703ac
Ayush Shekhar <ayush221b@gmail.com>
2019-03-14 15:04:11 +0530

    Updates

---
+++ precache-manifest.cbb888be83510edcf7149bc75f86f078.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20bf77da42ead4f12947",
+    "url": "/phonebook-app/static/css/main.5d7ea45e.chunk.css"
+  },
+  {
+    "revision": "20bf77da42ead4f12947",
+    "url": "/phonebook-app/static/js/main.a358a523.chunk.js"
+  },
+  {
+    "revision": "3c1f5d9d49453c68431e",
+    "url": "/phonebook-app/static/js/runtime~main.29b884d1.js"
+  },
+  {
+    "revision": "a3ccce24c387fc92afdb",
+    "url": "/phonebook-app/static/js/2.b15b160c.chunk.js"
+  },
+  {
+    "revision": "d8e9b0162cf6ab690abd7cb9362283e1",
+    "url": "/phonebook-app/index.html"
+  }
+];
